1b70f27c91a6fe251d3abd2c23e15db0970f8437
Martin Rosa <Martin.Rosa@gmail.com>
2025-09-13 20:08:26 -0400

    Update

---
+++ js/umami.js
@@ -0,0 +1,8 @@
+// docs/js/umami.js
+(function() {
+    var s = document.createElement('script');
+    s.defer = true;
+    s.src = "https://cloud.umami.is/script.js";
+    s.dataset.websiteId = "65b7c17f-597d-4654-8c4f-aac1efbbb10b";
+    document.head.appendChild(s);
+})();
